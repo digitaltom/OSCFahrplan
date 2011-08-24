21b0ea3dca47b4fa47945dda8a27caf6747783a8
Thomas Schmidt <tschmidt@suse.de>
2011-08-24 17:07:57 +0200

    some fixes for converter

--- libs/IndicoXmlConverter/convert.rb
+++ libs/IndicoXmlConverter/convert.rb
@@ -16,7 +16,7 @@ def escape_xml(input)
      when '<' then return '&lt;'
      when '>' then return '&gt;'
      when "'" then return '&apos;'
-     when '"' then return '&quote;'
+     when '"' then return '&quot;'
      end
    end
    return result
@@ -29,7 +29,8 @@ indico_doc = XML::Document.string( indico_string )
 schedule_doc = XML::Document.string( "<schedule/>" )
 
 # Creating header
-schedule_conference = schedule_doc.root << XML::Node.new( "conference" )
+schedule_conference = XML::Node.new( "conference" )
+schedule_doc.root << schedule_conference
 
 schedule_conference << XML::Node.new("title", "openSUSE Conference 2011" )
 schedule_conference << XML::Node.new("subtite", "rwx3" )
@@ -38,9 +39,9 @@ schedule_conference << XML::Node.new("city", "Nuemberg, Germany" )
 schedule_conference << XML::Node.new("start", "2011-09-11" )
 schedule_conference << XML::Node.new("end", "2011-09-14" )
 schedule_conference << XML::Node.new("days", "4" )
-schedule_conference << XML::Node.new("release", Time.now.to_s )
+schedule_conference << XML::Node.new("release", Time.now.strftime("Version %m/%d/%Y %H:%M") )
 schedule_conference << XML::Node.new("day_change", "00:00" )
-schedule_conference << XML::Node.new("timeslot_duration", "00:15" )
+schedule_conference << XML::Node.new("timeslot_duration", "00:30" )
 
 
 talks = indico_doc.find "//session/contribution"  
@@ -73,24 +74,25 @@ dates.each_with_index do |date, i|
       minutes = (((t2 - t1).to_i/60)%60).to_s.rjust(2, '0')
       event << XML::Node.new("duration", "#{hours}:#{minutes}" )
       event << XML::Node.new("room", talk.find_first( "location/room" ).content )
-      event << XML::Node.new("slug", "" )
+      event << XML::Node.new("slug", " " )
       event << XML::Node.new("title", escape_xml( talk.find_first( "title" ).content ) )
-      event << XML::Node.new("subtitle", "" )
+      event << XML::Node.new("subtitle", " " )
       track = talk.find_first( "track" ).nil? ? "unknown" : talk.find_first( "track" ).content
       event << XML::Node.new("track", track )
       type = talk.find_first( "type/name" ).nil? ? "unknown" : talk.find_first( "type/name" ).content
       event << XML::Node.new("type", type )
       event << XML::Node.new("language", "en" )
       event << XML::Node.new("abstract", escape_xml( talk.find_first( "abstract" ).content ) )
-      event << XML::Node.new("description", "" )
-      persons = event << XML::Node.new("persons")
+      event << XML::Node.new("description", " " )
+      persons = XML::Node.new("persons")
+      event << persons
       talk.find( "speakers/user" ).each do |speaker|
         name = "#{speaker.find_first( "name" )['first']} #{speaker.find_first( "name" )['last']}"
         person = XML::Node.new("person", name )
         person["id"] = ""
         persons << person
       end
-      event << XML::Node.new("links", "" )
+      event << XML::Node.new("links", " " )
 
       room << event
     end
